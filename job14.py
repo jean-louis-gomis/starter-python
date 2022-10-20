e2e35c46ef16c0a1f3707746511e002d5766587e
jean-louis-gomis <114387379+jean-louis-gomis@users.noreply.github.com>
2022-10-20 09:20:28 +0200

    Add files via upload

--- job14.py
+++ job14.py
@@ -1,21 +1,28 @@
-# Créer un programme job14.py. Le programme devra contenir une fonction myFunction()
-# qui écrit dans le terminal “je suis une fonction”.
+# # Créer un programme job14.py. Le programme devra contenir une fonction myFunction()
+# # qui écrit dans le terminal “je suis une fonction”.
 
-from operator import truediv
+# from operator import truediv
 
 
-permutation = True
-passage = 0
+# permutation = True
+# passage = 0
 
-while permutation == True:
-    permutation = False
-    passage = passage + 1
+# while permutation == True:
+#     permutation = False
+#     passage = passage + 1
 
-    for en_cours in range(0, len(myList) - passage):
-        if myList[en-cours] > myList[en_cours + 1]:
-            permutation = True
-            #On echange les deux éléments
-            myList[en_cours], myList[en_cours + 1] = \
-            myList[en_cours + 1],myList[en_cours]
+#     for en_cours in range(0, len(myList) - passage):
+#         if myList[en-cours] > myList[en_cours + 1]:
+#             permutation = True
+#             #On echange les deux éléments
+#             myList[en_cours], myList[en_cours + 1] = \
+#             myList[en_cours + 1],myList[en_cours]
 
-print (myList)            
+# print (myList)            
+
+
+
+def myFunction():
+    print("je suis une fonction")
+
+myFunction()
